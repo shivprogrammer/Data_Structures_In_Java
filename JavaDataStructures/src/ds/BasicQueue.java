14ebee45ff79bc715b25d20b913df840cd7daa1c
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-11 03:39:05 -0700

    laying out the basic methods for queue

--- JavaDataStructures/src/ds/BasicQueue.java
+++ JavaDataStructures/src/ds/BasicQueue.java
@@ -1,5 +1,23 @@
 package ds;
 
-public class BasicQueue {
+public class BasicQueue<X> {
+    public BasicQueue() {
+        X data = new BasicQueue[1000];
+    }
     
+    public void enqueue(X newItem) {
+        
+    }
+    
+    public X dequeue() {
+        
+    }
+    
+    public boolean search(X item) {
+        
+    }
+    
+    public X access(X item) {
+        
+    }
 }
