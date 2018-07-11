fd0e376d45a563b2a6d5c3590c7fdd08a8450487
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-10 20:31:15 -0700

    adding internal data to BasicStack class

--- JavaDataStructures/src/javadatastructures/BasicStack.java
+++ JavaDataStructures/src/javadatastructures/BasicStack.java
@@ -1,4 +1,5 @@
 package javadatastructures;
 
 public class BasicStack<X> { // <X> is saying that we can type this class to anything generically right now but specifically later on.
+    private X [] data; // Keep scope private so outside classes don't use the underlying data structure directly. Since we don't want it used we don't add any getters or setters to it.
 }
