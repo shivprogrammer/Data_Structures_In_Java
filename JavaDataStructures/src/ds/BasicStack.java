40d79c377047745cc3ec09a59a73d809f7949da5
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-11 00:42:51 -0700

    moved BasicStack to new ds package

--- JavaDataStructures/src/ds/BasicStack.java
+++ JavaDataStructures/src/ds/BasicStack.java
@@ -1,4 +1,4 @@
-package javadatastructures;
+package ds;
 
 public class BasicStack<X> { // <X> is saying that we can type this class to anything generically right now but specifically later on.
     private X [] data; // Keep scope private so outside classes don't use the underlying data structure directly. Since we don't want it used we don't add any getters or setters to it.
