a56527568f906d88fa5fca3d4195c4582b02f94b
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-10 20:42:05 -0700

    Adding in stackPointer variable and utilizing it with the constructor & push method

--- JavaDataStructures/src/javadatastructures/BasicStack.java
+++ JavaDataStructures/src/javadatastructures/BasicStack.java
@@ -2,16 +2,20 @@ package javadatastructures;
 
 public class BasicStack<X> { // <X> is saying that we can type this class to anything generically right now but specifically later on.
     private X [] data; // Keep scope private so outside classes don't use the underlying data structure directly. Since we don't want it used we don't add any getters or setters to it.
+    private int stackPointer;
     
     public BasicStack() {
         data = (X[]) new Object[1000];
+        stackPointer = 0;
     }
     
     public void push(X newItem) {
-        
+        data[stackPointer++] = newItem;
     }
     
     public X pop() {
         
     }
+    
+    
 }
