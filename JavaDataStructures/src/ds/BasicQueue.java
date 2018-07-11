46558e6ccea9f0bee7bab4adab600569b8e018ef
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-11 05:04:19 -0700

    built out size method for queue

--- JavaDataStructures/src/ds/BasicQueue.java
+++ JavaDataStructures/src/ds/BasicQueue.java
@@ -2,25 +2,38 @@ package ds;
 
 public class BasicQueue<X> {
     private X[] data;
-    private int queuePointer = 0;
+    private int front;
+    private int end;
     
     public BasicQueue() {
         this(1000);
     }
     
     public BasicQueue(int size) {
+        this.front = -1;
+        this.end = -1;
         data = (X[])new Object[size];
     }
     
+    public int size() {
+        if (front == -1 && end == -1) {
+            return 0;
+        }
+        else {
+            return end - front + 1;
+        }
+    }
+    
     public void enqueue(X newItem) {
-        data[queuePointer++] = newItem;
+        front = 0;
+        data[end++] = newItem;
         queuePointer++;
         
     }
     
     public X dequeue() {
-        queuePointer--;
-        return data[0];
+        front++;
+        return data[front - 1];
         
     }
     
