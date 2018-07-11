258231db5e5c3cfa03e988921b35c40f3ed12670
shivprogrammer <shiv.unadkat@gmail.com>
2018-07-10 20:32:46 -0700

    added BasicStack constructor

--- JavaDataStructures/src/javadatastructures/BasicStack.java
+++ JavaDataStructures/src/javadatastructures/BasicStack.java
@@ -2,4 +2,8 @@ package javadatastructures;
 
 public class BasicStack<X> { // <X> is saying that we can type this class to anything generically right now but specifically later on.
     private X [] data; // Keep scope private so outside classes don't use the underlying data structure directly. Since we don't want it used we don't add any getters or setters to it.
+    
+    public BasicStack() {
+        data = (X[]) new Object[1000];
+    }
 }
